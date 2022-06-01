d776ab115b129530a85ee71f901849cf4489b1cb
CeciGarlop <010.ceci@gmail.com>
2022-05-31 20:35:28 -0500

    Primera practica

---
+++ 0_IntroJS/main.js
@@ -0,0 +1,23 @@
+//Js es un lenguaje que se lee de arriba a abajo
+
+//SALIDAS
+
+//Cómo poner una alerta desde JS
+/*alert('Hola, mundo!')
+alert('Otro alert')*/
+
+//Enviar mensajes a la consola
+console.log('este mensaje se verá en la consola')
+console.log('Otro mensaje')
+
+//Mostrar un error en la consola
+console.error('ERROR:No se encontró el archivo')
+
+//Mostrar un warning en la consola
+console.warn('Estás añadiendo muchas alertas')
+
+document.write('Texto desde JS')
+
+//ENTRADA
+/*prompt('Introduce tu nombre')
+prompt('Introduce tu apellido')*/
